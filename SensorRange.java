cdfa8a786bf0edc7978e263db279ae1ccea1b6ee
glitchyxlatrell <la586421@ucf.edu>
2026-01-29 02:02:43 +0000

    minor change

---
+++ SensorRange.java
@@ -0,0 +1,19 @@
+/*  Latrell Kong
+    Assignment 1 - SensorRange
+    COP3503 Computer Science 2
+    SensorRange.java
+*/
+
+public class SensorRange
+{
+    public static int[] rangeCountBF(int[] readings, int[][] queries);
+    {
+
+    }
+
+    public static int[] rangeCountFast(int[] readings, int[][] queries);{
+        
+    }
+
+
+}
