a245486ce779c3fc3e458dc0d39a7596102738f4
glitchyxlatrell <la586421@ucf.edu>
2026-01-29 02:18:07 +0000

    completing bruteForce method

--- SensorRange.java
+++ SensorRange.java
@@ -6,12 +6,41 @@
 
 public class SensorRange
 {
-    public static int[] rangeCountBF(int[] readings, int[][] queries);
+    public static int[] rangeCountBF(int[] readings, int[][] queries)
     {
+        int queryLength = queries.length;
+        int returnArray[] = new int[queryLength];
+        int leftBound = 0;
+        int rightBound = 0;
+        
+        for(int i = 0; i < queryLength; i++)
+        {   
+            int queryCounter = 0;
+            leftBound = queries[i][0];
+            rightBound = queries[i][1];
 
+            if(leftBound > rightBound)
+            {
+                returnArray[i] = 0;
+                continue;
+            }
+
+            for(int j = 0; j < readings.length; j++)
+            {
+                if(readings[j] >= leftBound && readings[j] <= rightBound)
+                    {
+                    queryCounter++;
+                }
+            }
+
+            returnArray[i] = queryCounter;
+        }
+
+        return returnArray;
     }
 
-    public static int[] rangeCountFast(int[] readings, int[][] queries);{
+    public static int[] rangeCountFast(int[] readings, int[][] queries)
+    {
         
     }
 
