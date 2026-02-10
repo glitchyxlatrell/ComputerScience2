41071630d10cb507296d10f2edb038d826f2a2a5
glitchyxlatrell <la586421@ucf.edu>
2026-02-10 00:16:57 +0000

    setting up to start coding assignment 2 for cs2

---
+++ SudokuSolver.java
@@ -0,0 +1,15 @@
+
+/*  Latrell Kong
+    Mystic Sudoku
+    COP3503 Computer Science 2
+    SudokuSolver.java
+*/
+
+public class SudokuSolver
+{
+    public int solve(int[][] board, int[][] forbiddenPairs)
+    {
+        
+    }
+
+}
