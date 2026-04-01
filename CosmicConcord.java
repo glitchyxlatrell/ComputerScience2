09a435add0678fad911f89fee22d782370293166
glitchyxlatrell <la586421@ucf.edu>
2026-04-01 02:29:49 +0000

    setting up for logic

--- CosmicConcord.java
+++ CosmicConcord.java
@@ -3,3 +3,26 @@
     COP3503 Computer Science 2
     CosmicConcord.java
 */
+
+public class CosmicConcord
+{
+    public static int solveRecMemo(int[] A, int[] B, int N, int M, int D, int G, int K)
+    {
+        return 0;
+    }
+
+    public static int solveTab(int[] A, int[] B, int N, int M, int D, int G, int K)
+    {
+        return 0;
+    }
+
+
+
+    public static void solve(int[] A, int[] B, int N, int M, int D, int G, int K)
+    {  
+
+        // printing out results of solving with memoization and tabulation
+        System.out.println("Maximum length using Recursion (with Memoization): " + solveRecMemo(A, B, N, M, D, G, K)); 
+		System.out.println("Maximum length using Tabulation: " + solveTab(A, B, N, M, D, G, K)); 
+    }
+}
